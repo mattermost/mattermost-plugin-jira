b3b6222b5f6c08e2b7e7eab0e1d54e82e300a215
JoramWilander <jwawilander@gmail.com>
2018-08-16 18:06:44 -0400

    Add the ability to connect to a user account

--- server/plugin.go
+++ server/plugin.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/subtle"
 	"encoding/json"
 	"fmt"
@@ -13,10 +14,14 @@ import (
 
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
+
+	jira "github.com/andygrunwald/go-jira"
+	jwt "golang.org/x/oauth2/jira"
 )
 
 const (
 	KEY_SECURITY_CONTEXT = "security_context"
+	KEY_USER_INFO        = "user_info_"
 )
 
 type Plugin struct {
@@ -40,6 +45,7 @@ type SecurityContext struct {
 	ProductType    string `json:"productType"`
 	Description    string `json:"description"`
 	EventType      string `json:"eventType"`
+	OAuthClientId  string `json:"oauthClientId"`
 }
 
 func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
@@ -49,6 +55,15 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 	}
 
 	switch r.URL.Path {
+	case "/test":
+		p.serveTest(w, r)
+		return
+	case "/connect":
+		p.serveUserConnectPage(w, r)
+		return
+	case "/connect/complete":
+		p.serveUserConnectComplete(w, r)
+		return
 	case "/webhook":
 		p.serveWebhook(w, r)
 		return
@@ -64,6 +79,71 @@ func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Req
 	}
 
 	http.NotFound(w, r)
+}
+
+func (p *Plugin) serveUserConnectPage(w http.ResponseWriter, r *http.Request) {
+	jiraURL := r.URL.Query().Get("xdm_e")
+
+	config := p.API.GetConfig()
+	completeURL := *config.ServiceSettings.SiteURL + "/" + path.Join("plugins", PluginId, "connect", "complete")
+
+	html := `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<script src="%s/atlassian-connect/all.js"></script>
+			<script>
+				AP.getCurrentUser(function(user){
+					console.log("user id:", user.atlassianAccountId);
+					window.open("%s?account_id=" + user.atlassianAccountId);
+				});
+			</script>
+		</head>
+		<body>
+			<p>From the Mattermost JIRA plugin.</p>
+		</body>
+	</html>
+	`
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(fmt.Sprintf(html, jiraURL, completeURL)))
+}
+
+type JiraUserInfo struct {
+	AccountId string
+}
+
+func (p *Plugin) serveUserConnectComplete(w http.ResponseWriter, r *http.Request) {
+	userID := r.Header.Get("Mattermost-User-ID")
+	if userID == "" {
+		http.Error(w, "Not authorized", http.StatusUnauthorized)
+		return
+	}
+
+	info := &JiraUserInfo{AccountId: r.URL.Query().Get("account_id")}
+
+	if info.AccountId == "" {
+		http.Error(w, "Missing account_id", http.StatusBadRequest)
+		return
+	}
+
+	b, _ := json.Marshal(info)
+
+	p.API.KVSet(KEY_USER_INFO+userID, b)
+
+	jiraClient, err := p.getJIRAClientForUser(info.AccountId)
+	if err != nil {
+		http.Error(w, "could not get jira client, err="+err.Error(), 500)
+	}
+
+	user, _, err := jiraClient.User.GetSelf()
+	if err != nil {
+		http.Error(w, "could not get the user, err="+err.Error(), 500)
+	}
+
+	userBytes, _ := json.Marshal(user)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(userBytes)
 }
 
 func (p *Plugin) serveAtlassianConnect(w http.ResponseWriter, r *http.Request) {
@@ -90,6 +170,7 @@ func (p *Plugin) serveInstalled(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 		return
 	}
+
 	var sc SecurityContext
 	json.Unmarshal(body, &sc)
 
@@ -98,6 +179,73 @@ func (p *Plugin) serveInstalled(w http.ResponseWriter, r *http.Request) {
 	p.API.KVSet(KEY_SECURITY_CONTEXT, body)
 
 	json.NewEncoder(w).Encode([]string{"OK"})
+}
+
+func (p *Plugin) getJiraUserInfo(userID string) (*JiraUserInfo, error) {
+	b, _ := p.API.KVGet(KEY_USER_INFO + userID)
+	if b == nil {
+		return nil, fmt.Errorf("could not find jira user info")
+	}
+
+	info := JiraUserInfo{}
+	err := json.Unmarshal(b, &info)
+	if err != nil {
+		return nil, err
+	}
+
+	return &info, nil
+}
+
+func (p *Plugin) serveTest(w http.ResponseWriter, r *http.Request) {
+	userID := r.Header.Get("Mattermost-User-ID")
+	if userID == "" {
+		http.Error(w, "Not authorized", http.StatusUnauthorized)
+		return
+	}
+
+	info, err := p.getJiraUserInfo(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	jiraClient, err := p.getJIRAClientForUser(info.AccountId)
+	if err != nil {
+		http.Error(w, "could not get jira client, err="+err.Error(), 500)
+	}
+
+	user, _, err := jiraClient.User.GetSelf()
+	if err != nil {
+		http.Error(w, "could not get the user, err="+err.Error(), 500)
+	}
+
+	userBytes, _ := json.Marshal(user)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(userBytes)
+}
+
+func (p *Plugin) loadSecurityContext() {
+	b, _ := p.API.KVGet(KEY_SECURITY_CONTEXT)
+	var sc SecurityContext
+	json.Unmarshal(b, &sc)
+	p.securityContext = &sc
+}
+
+func (p *Plugin) getJIRAClientForUser(jiraUser string) (*jira.Client, error) {
+	if p.securityContext == nil {
+		p.loadSecurityContext()
+	}
+
+	c := jwt.Config{
+		BaseURL: p.securityContext.BaseURL,
+		Subject: jiraUser,
+	}
+
+	c.Config.ClientID = p.securityContext.OAuthClientId
+	c.Config.ClientSecret = p.securityContext.SharedSecret
+	c.Config.Endpoint.AuthURL = "https://auth.atlassian.io"
+	c.Config.Endpoint.TokenURL = "https://auth.atlassian.io/oauth2/token"
+
+	return jira.NewClient(c.Client(context.Background()), c.BaseURL)
 }
 
 func (p *Plugin) serveIssueEvent(w http.ResponseWriter, r *http.Request) {
