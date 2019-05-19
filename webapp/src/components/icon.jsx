7fdda4220aa47fa1e13c13a76fbdd9d5f4d9f7fc
Asaad Mahmood <asaad@mattermost.com>
2019-05-19 15:07:16 +0500

    Updating jira icon

--- webapp/src/components/icon.jsx
+++ webapp/src/components/icon.jsx
@@ -4,8 +4,14 @@
 import React from 'react';
 
 export const ChannelHeaderButtonIcon = () => (
-    <i
-        className='icon fa fa-angle-double-up'
-        style={{fontSize: '15px', position: 'relative', top: '-1px'}}
-    />
+    <svg
+        aria-hidden='true'
+        focusable='false'
+        role='img'
+        viewBox='0 0 496 512'
+        width='14'
+        height='14'
+    >
+        <path d='M490 241.7C417.1 169 320.6 71.8 248.5 0 83 164.9 6 241.7 6 241.7c-7.9 7.9-7.9 20.7 0 28.7C138.8 402.7 67.8 331.9 248.5 512c379.4-378 15.7-16.7 241.5-241.7 8-7.9 8-20.7 0-28.6zm-241.5 90l-76-75.7 76-75.7 76 75.7-76 75.7z'/>
+    </svg>
 );
