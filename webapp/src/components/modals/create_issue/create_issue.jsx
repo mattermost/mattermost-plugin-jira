55a16f9198e3c7455dc9f4e2f3ecfc154ef2ba50
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2019-05-24 20:44:29 +0500

    MM-15535 - UI updates for Jira 2 (#71)
    * MM-15535 - UI updates for Jira 2
    
    * Reverting input class
    
    * Updating textarea
    
    * Removing readonly
    
    * Updating readonly changes

--- webapp/src/components/modals/create_issue/create_issue.jsx
+++ webapp/src/components/modals/create_issue/create_issue.jsx
@@ -205,7 +205,7 @@ export default class CreateIssueModal extends PureComponent {
                     <Modal.Footer>
                         <FormButton
                             type='button'
-                            btnClass='btn-default'
+                            btnClass='btn-link'
                             defaultMessage='Cancel'
                             onClick={this.handleClose}
                         />
