1d055c4d63a4fa5a51ea0daa554f5b5512bbe893
Christopher Poile <cpoile@gmail.com>
2019-06-01 15:26:45 -0400

    refactored theme styling, reduced duplication

--- webapp/src/components/jira_issue_selector/jira_issue_selector.jsx
+++ webapp/src/components/jira_issue_selector/jira_issue_selector.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import debounce from 'debounce-promise';
 import AsyncSelect from 'react-select/lib/Async';
 
-import {changeOpacity} from 'mattermost-redux/utils/theme_utils';
+import {getStyleForReactSelect} from '../utils';
 
 const searchDefaults = 'ORDER BY updated DESC';
 const searchDebounceDelay = 400;
@@ -38,62 +38,6 @@ export default class JiraIssueSelector extends Component {
                 return json;
             });
     };
-
-    getStyle = (theme) => ({
-        menuPortal: (provided) => ({
-            ...provided,
-            zIndex: 9999,
-        }),
-        control: (provided, state) => ({
-            ...provided,
-            color: theme.centerChannelColor,
-            background: theme.centerChannelBg,
-
-            // Overwrittes the different states of border
-            borderColor: state.isFocused ? changeOpacity(theme.centerChannelColor, 0.25) : changeOpacity(theme.centerChannelColor, 0.12),
-
-            // Removes weird border around container
-            boxShadow: 'inset 0 1px 1px ' + changeOpacity(theme.centerChannelColor, 0.075),
-            borderRadius: '2px',
-
-            '&:hover': {
-                borderColor: changeOpacity(theme.centerChannelColor, 0.25),
-            },
-        }),
-        option: (provided, state) => ({
-            ...provided,
-            background: state.isSelected ? changeOpacity(theme.centerChannelColor, 0.12) : theme.centerChannelBg,
-            color: theme.centerChannelColor,
-            '&:hover': {
-                background: changeOpacity(theme.centerChannelColor, 0.12),
-            },
-        }),
-        menu: (provided) => ({
-            ...provided,
-            color: theme.centerChannelColor,
-            background: theme.centerChannelBg,
-            border: '1px solid ' + changeOpacity(theme.centerChannelColor, 0.2),
-            borderRadius: '0 0 2px 2px',
-            boxShadow: changeOpacity(theme.centerChannelColor, 0.2) + ' 1px 3px 12px',
-            marginTop: '4px',
-        }),
-        placeholder: (provided) => ({
-            ...provided,
-            color: theme.centerChannelColor,
-        }),
-        dropdownIndicator: (provided) => ({
-            ...provided,
-            color: changeOpacity(theme.centerChannelColor, 0.4),
-        }),
-        singleValue: (provided) => ({
-            ...provided,
-            color: theme.centerChannelColor,
-        }),
-        indicatorSeparator: (provided) => ({
-            ...provided,
-            display: 'none',
-        }),
-    });
 
     debouncedSearchIssues = debounce(this.searchIssues, searchDebounceDelay);
 
@@ -127,7 +71,7 @@ export default class JiraIssueSelector extends Component {
                     defaultOptions={true}
                     loadOptions={this.handleIssueSearchTermChange}
                     menuPortalTarget={document.body}
-                    styles={this.getStyle(this.props.theme)}
+                    styles={getStyleForReactSelect(this.props.theme)}
                 />
                 <div className={'help-text'}>
                     {'Returns issues sorted by most recently updated.'} <br/>
---
+++ webapp/src/components/utils.jsx
@@ -0,0 +1,66 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {changeOpacity} from 'mattermost-redux/utils/theme_utils';
+
+export const getStyleForReactSelect = (theme) => {
+    if (!theme) {
+        return null;
+    }
+
+    return {
+        menuPortal: (provided) => ({
+            ...provided,
+            zIndex: 9999,
+        }),
+        control: (provided, state) => ({
+            ...provided,
+            color: theme.centerChannelColor,
+            background: theme.centerChannelBg,
+
+            // Overwrittes the different states of border
+            borderColor: state.isFocused ? changeOpacity(theme.centerChannelColor, 0.25) : changeOpacity(theme.centerChannelColor, 0.12),
+
+            // Removes weird border around container
+            boxShadow: 'inset 0 1px 1px ' + changeOpacity(theme.centerChannelColor, 0.075),
+            borderRadius: '2px',
+
+            '&:hover': {
+                borderColor: changeOpacity(theme.centerChannelColor, 0.25),
+            },
+        }),
+        option: (provided, state) => ({
+            ...provided,
+            background: state.isSelected ? changeOpacity(theme.centerChannelColor, 0.12) : theme.centerChannelBg,
+            color: theme.centerChannelColor,
+            '&:hover': {
+                background: changeOpacity(theme.centerChannelColor, 0.12),
+            },
+        }),
+        menu: (provided) => ({
+            ...provided,
+            color: theme.centerChannelColor,
+            background: theme.centerChannelBg,
+            border: '1px solid ' + changeOpacity(theme.centerChannelColor, 0.2),
+            borderRadius: '0 0 2px 2px',
+            boxShadow: changeOpacity(theme.centerChannelColor, 0.2) + ' 1px 3px 12px',
+            marginTop: '4px',
+        }),
+        placeholder: (provided) => ({
+            ...provided,
+            color: theme.centerChannelColor,
+        }),
+        dropdownIndicator: (provided) => ({
+            ...provided,
+            color: changeOpacity(theme.centerChannelColor, 0.4),
+        }),
+        singleValue: (provided) => ({
+            ...provided,
+            color: theme.centerChannelColor,
+        }),
+        indicatorSeparator: (provided) => ({
+            ...provided,
+            display: 'none',
+        }),
+    };
+};
