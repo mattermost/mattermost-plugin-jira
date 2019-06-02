4c81a7fb7bf6f5112d9febcb41c9359d600a3bce
Asaad Mahmood <asaad@mattermost.com>
2019-06-03 03:31:37 +0500

    Updating code

--- webapp/src/components/react_select_setting.jsx
+++ webapp/src/components/react_select_setting.jsx
@@ -19,7 +19,7 @@ export default class ReactSelectSetting extends React.PureComponent {
 
     handleChange = (value) => {
         if (this.props.onChange) {
-                this.props.onChange(this.props.name, value.value);
+            if (Array.isArray(value)) {
                 this.props.onChange(this.props.name, value.map((x) => x.value));
             } else {
                 this.props.onChange(this.props.name, value.value);
