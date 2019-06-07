95185e6c9034875746c09cb6227e5dc771eac7a5
jfrerich <jasonfrerich@yahoo.com>
2019-06-07 11:04:11 -0500

    [MM-15913] Update UX when creating issues for projects that require additional fields (#119)
    * - new prop in json object tells server to post link to create issue page
      in jira.
    - getFieldsNotCovered() determines if any required Jira fields are
      outside realm of fields and returns array of fieldnames not
      supported
      - compares jirametadata fields to hardcoded list of fieldnames
        currently supported
      - currently passes arrays as props to jiraFields. will remove
    - Issue with adding jiraIssue Struct as encoded query in link
      - query attributes are Capitalized and link requires lower case.
        (mostly). example Priority needs to be priority
      - write / find something to convert all struct keys to
        lowercase
      - can hardcode to get current set of supported fields working and fix
        in Jira 2.1
      - Link is posting to MM, but query params need to be automated
        - trying to use populated jiraIssue struct
    
    * fill query string from allowed fields for now
      - refactor to its own function
    
    * Small clean up and nit fix from review
    Break up if statement for custom fields and allowed field names. Was
    easier for debugging
    
    * - convert message to ephemeral post when user non handled required fields
      are found
    - add non-handled required fields as list in message
    
    * for ephemeeral message, bost as Jira Bot, not system
    
    * Remove extra period after colon
    
    * Get botUserId through getConfig Method, not actual field name (conf)
    
    * remove console.log.  Was causing make test / build failure

--- webapp/src/components/modals/create_issue/create_issue.jsx
+++ webapp/src/components/modals/create_issue/create_issue.jsx
@@ -62,7 +62,50 @@ export default class CreateIssueModal extends PureComponent {
         }
     }
 
+    allowedFields = [
+        'project',
+        'issuetype',
+        'priority',
+        'description',
+        'summary',
+    ]
+
+    allowedSchemaCustom = [
+        'com.atlassian.jira.plugin.system.customfieldtypes:textarea',
+        'com.atlassian.jira.plugin.system.customfieldtypes:textfield',
+        'com.atlassian.jira.plugin.system.customfieldtypes:select',
+        'com.atlassian.jira.plugin.system.customfieldtypes:project',
+
+        // 'com.pyxis.greenhopper.jira:gh-epic-link',
+
+        // epic label is 'Epic Name' for cloud instance
+        'com.pyxis.greenhopper.jira:gh-epic-label',
+    ];
+
+    getFieldsNotCovered() {
+        const {jiraIssueMetadata} = this.props;
+        const myfields = getFields(jiraIssueMetadata, this.state.projectKey, this.state.issueType);
+
+        const fieldsNotCovered = [];
+
+        Object.keys(myfields).forEach((key) => {
+            if (myfields[key].required) {
+                if ((!myfields[key].schema.custom && !this.allowedFields.includes(key)) ||
+                     (myfields[key].schema.custom && !this.allowedSchemaCustom.includes(myfields[key].schema.custom))
+                ) {
+                    if (!fieldsNotCovered.includes(key)) {
+                        // fieldsNotCovered.push([key, myfields[key].name]);
+                        fieldsNotCovered.push(myfields[key].name);
+                    }
+                }
+            }
+        });
+        return fieldsNotCovered;
+    }
+
     handleCreate = (e) => {
+        const requiredFieldsNotCovered = this.getFieldsNotCovered();
+
         if (e && e.preventDefault) {
             e.preventDefault();
         }
@@ -75,6 +118,7 @@ export default class CreateIssueModal extends PureComponent {
             current_team: this.props.currentTeam.name,
             fields: this.state.fields,
             channel_id: channelId,
+            required_fields_not_covered: requiredFieldsNotCovered,
         };
 
         this.setState({submitting: true});
@@ -191,6 +235,8 @@ export default class CreateIssueModal extends PureComponent {
                         fields={getFields(jiraIssueMetadata, this.state.projectKey, this.state.issueType)}
                         onChange={this.handleFieldChange}
                         values={this.state.fields}
+                        allowedFields={this.allowedFields}
+                        allowedSchemaCustom={this.allowedSchemaCustom}
                         theme={theme}
                     />
                     <br/>
