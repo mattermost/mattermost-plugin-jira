2cd949cbad2723018a87569b8dc580ee9513334d
Lev Brouk <lev@mattermost.com>
2019-06-06 19:00:58 -0700

    PR feedback, return 200 on no permissions, with an error

--- server/issue.go
+++ server/issue.go
@@ -186,18 +186,17 @@ func httpAPIGetCreateIssueMetadata(ji Instance, w http.ResponseWriter, r *http.R
 
 	w.Header().Set("Content-Type", "application/json")
 
+	var bb []byte
 	if len(cimd.Projects) == 0 {
-		fmt.Fprintf(w, "{\"error\": \"You do not have permission to create issues in any projects. Please contact your Jira admin.\"}")
-		return http.StatusInternalServerError,
-			errors.WithMessage(err, "failed to marshal response")
+		bb = []byte(`{"error": "You do not have permission to create issues in any projects. Please contact your Jira admin."}`)
+	} else {
+		bb, err = json.Marshal(cimd)
+		if err != nil {
+			return http.StatusInternalServerError,
+				errors.WithMessage(err, "failed to marshal response")
+		}
 	}
-
-	b, err := json.Marshal(cimd)
-	if err != nil {
-		return http.StatusInternalServerError,
-			errors.WithMessage(err, "failed to marshal response")
-	}
-	_, err = w.Write(b)
+	_, err = w.Write(bb)
 	if err != nil {
 		return http.StatusInternalServerError,
 			errors.WithMessage(err, "failed to write response")
