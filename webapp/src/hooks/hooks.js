7473020b83a8e8543bd2d916920ec05e0e4f32ef
Christopher Poile <cpoile@gmail.com>
2019-06-19 18:45:47 -0400

    check if the instance is connected

--- webapp/src/hooks/hooks.js
+++ webapp/src/hooks/hooks.js
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 import {openCreateModalWithoutPost, sendEphemeralPost} from '../actions';
-import {isUserConnected} from '../selectors';
+import {isUserConnected, isInstanceInstalled} from '../selectors';
 
 export default class Hooks {
     constructor(store) {
@@ -11,6 +11,10 @@ export default class Hooks {
 
     slashCommandWillBePostedHook = (message, contextArgs) => {
         if (message && (message.startsWith('/jira create ') || message === '/jira create')) {
+            if (!isInstanceInstalled(this.store.getState())) {
+                sendEphemeralPost(this.store, 'There is not Jira instance installed. Please contact your system administrator.');
+                return Promise.resolve({});
+            }
             if (!isUserConnected(this.store.getState())) {
                 sendEphemeralPost(this.store, 'Your username is not connected to Jira. Please type `/jira connect`.');
                 return Promise.resolve({});
