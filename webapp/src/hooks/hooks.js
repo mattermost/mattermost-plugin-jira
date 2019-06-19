ca24eb521a98d305a2214f93be30251ab7dbe85d
Christopher Poile <cpoile@gmail.com>
2019-06-19 17:04:18 -0400

    [MM-15786] check on the client-side whether user is connected

--- webapp/src/hooks/hooks.js
+++ webapp/src/hooks/hooks.js
@@ -1,7 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {openCreateModalWithoutPost} from '../actions';
+import {openCreateModalWithoutPost, sendEphemeralPost} from '../actions';
+import {isUserConnected} from '../selectors';
 
 export default class Hooks {
     constructor(store) {
@@ -10,6 +11,10 @@ export default class Hooks {
 
     slashCommandWillBePostedHook = (message, contextArgs) => {
         if (message && (message.startsWith('/jira create ') || message === '/jira create')) {
+            if (!isUserConnected(this.store.getState())) {
+                sendEphemeralPost(this.store, 'Your username is not connected to Jira. Please type `/jira connect`.');
+                return Promise.resolve({});
+            }
             const description = message.slice(12).trim();
             this.store.dispatch(openCreateModalWithoutPost(description, contextArgs.channel_id));
             return Promise.resolve({});
