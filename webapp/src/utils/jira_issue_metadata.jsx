8c4c4f9c4968095f08ae3de6b61174684405dac3
mickmister <mjkochell@gmail.com>
2019-07-21 14:16:47 -0400

    Make code more inline

--- webapp/src/utils/jira_issue_metadata.jsx
+++ webapp/src/utils/jira_issue_metadata.jsx
@@ -62,15 +62,9 @@ export function getCustomFieldValuesForProjects(metadata, projectKeys) {
         }
     }
 
-    return Object.values(customFieldHash).sort((a, b) => {
-        if (a.name < b.name) {
-            return -1;
-        }
-        if (a.name > b.name) {
-            return 1;
-        }
-        return 0;
-    }).map((field) => ({
+    return Object.values(customFieldHash)
+    .sort((a, b) => {if (a.name < b.name) {return -1;} if (a.name > b.name) {return 1;} return 0;})
+    .map((field) => ({
         label: `Issue Updated: Custom - ${field.name}`,
         value: `event_updated_${field.key}`,
     }));
