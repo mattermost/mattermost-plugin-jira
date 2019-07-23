f16d2ed3b9596b322333e6dd9ee462ef44641bd8
Christopher Poile <cpoile@gmail.com>
2019-07-23 10:40:31 -0400

    [MM-17234] continue on errors that might cause problems later

--- server/webhook_worker.go
+++ server/webhook_worker.go
@@ -13,12 +13,22 @@ func (ww webhookWorker) work() {
 	for bb := range ww.workQueue {
 		wh, err := ParseWebhook(bb)
 		if err != nil {
-			ww.p.errorf("webhookWorker id: %d, error parsing webhook, err: %v", ww.id, err)
+			// Don't log an error if we're just ignoring the webhook.
+			if err != ErrWebhookIgnored {
+				ww.p.errorf("webhookWorker id: %d, error parsing webhook, err: %v", ww.id, err)
+			}
+			continue
+		}
+
+		_, _, err = wh.PostNotifications(ww.p)
+		if err != nil {
+			ww.p.errorf("WebhookWorker id: %d, error posting notifications, err: %v", ww.id, err)
 		}
 
 		channelIds, err := ww.p.getChannelsSubscribed(wh.(*webhook))
 		if err != nil {
 			ww.p.errorf("webhookWorker id: %d, error getting channel's subscribed, err: %v", ww.id, err)
+			continue
 		}
 
 		botUserId := ww.p.getUserID()
@@ -27,11 +37,6 @@ func (ww webhookWorker) work() {
 			if _, _, err1 := wh.PostToChannel(ww.p, channelId, botUserId); err1 != nil {
 				ww.p.errorf("WebhookWorker id: %d, error posting to channel, err: %v", ww.id, err)
 			}
-		}
-
-		_, _, err = wh.PostNotifications(ww.p)
-		if err != nil {
-			ww.p.errorf("WebhookWorker id: %d, error posting notifications, err: %v", ww.id, err)
 		}
 	}
 }
