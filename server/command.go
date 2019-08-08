2ac699b33d0371536758b227c1eeb2503ae44bf9
Michael Kochell <mjkochell@gmail.com>
2019-08-08 18:24:40 -0400

    Reword `/jira view` help text
    Co-Authored-By: Aaron Rothschild <aaron.rothschild@gmail.com>

--- server/command.go
+++ server/command.go
@@ -16,7 +16,7 @@ const helpText = "###### Mattermost Jira Plugin - Slash Command Help\n" +
 	"* `/jira assign <issue-key> <assignee>` - Change the assignee of a Jira issue\n" +
 	"* `/jira create <text (optional)>` - Create a new Issue with 'text' inserted into the description field\n" +
 	"* `/jira transition <issue-key> <state>` - Change the state of a Jira issue\n" +
-	"* `/jira view <issue-key>` - View a Jira issue\n" +
+	"* `/jira view <issue-key>` - View the details of a specific Jira issue\n" +
 	"* `/jira settings [setting] [value]` - Update your user settings\n" +
 	"  * [setting] can be `notifications`\n" +
 	"  * [value] can be `on` or `off`\n" +
