25215568a45b80840facabc4649e5ef524fda46b
Michael Kochell <mjkochell@gmail.com>
2019-08-28 11:03:29 -0400

    Make sure post headline says 'mentioned you' when you are the assignee (#297)

--- server/manifest.go
+++ server/manifest.go
@@ -7,5 +7,5 @@ var manifest = struct {
 }{
 	Id:      "jira",
 	Version: "2.1.0",
-	Hash:    "ba80f9f",
+	Hash:    "8e17cf0",
 }
--- webapp/src/manifest.js
+++ webapp/src/manifest.js
@@ -1,3 +1,3 @@
 export const id = 'jira';
 export const version = '2.1.0';
-export const hash = 'ba80f9f';
+export const hash = '8e17cf0';
