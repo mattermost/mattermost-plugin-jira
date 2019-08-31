d69fdcf6f5ed5d39c0aed0d93d7ba1fdbf33c50f
Lev Brouk <lev@mattermost.com>
2019-08-31 10:53:09 -0700

    Revert "* include hash in manifest files; output version and hash with info (#284)"
    This reverts commit edfb709808a233d11a956f49ccf52bc05168166a.

--- server/manifest.go
+++ server/manifest.go
@@ -5,7 +5,7 @@ var manifest = struct {
 	Version string
 	Hash    string
 }{
-	Id:      "generated",
-	Version: "generated",
-	Hash:    "generated",
+	Id:      "jira",
+	Version: "2.1.0",
+	Hash:    "cda4cdc",
 }
