86a5ca67c5a3b7ecad029fb5d31deb57bbfc24f4
Christopher Poile <cpoile@gmail.com>
2019-09-03 16:01:43 -0400

    [MM-17840] Add strike-through for none on single change messages (#292)
    * add strikethrough for single "None", and don't add it twice
    
    * hash

--- server/webhook_parser.go
+++ server/webhook_parser.go
@@ -123,7 +123,7 @@ func parseWebhookChangeLog(jwh *JiraWebhook) Webhook {
 		to := item.ToString
 		fromWithDefault := from
 		if fromWithDefault == "" {
-			fromWithDefault = "None"
+			fromWithDefault = "~~None~~"
 		}
 		toWithDefault := to
 		if toWithDefault == "" {
@@ -432,7 +432,7 @@ func mergeWebhookEvents(events []*webhook) Webhook {
 		merged.eventTypes = merged.eventTypes.Union(event.eventTypes)
 		strikePre := "~~"
 		strikePost := "~~"
-		if event.fieldInfo.name == "description" {
+		if event.fieldInfo.name == "description" || strings.HasPrefix(event.fieldInfo.from, "~~") {
 			strikePre = ""
 			strikePost = ""
 		}
