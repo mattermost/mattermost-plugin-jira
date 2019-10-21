bcb9856163e9df2a7ed79a3145815daf42c487fb
Lev <1187448+levb@users.noreply.github.com>
2019-10-21 13:00:06 -0700

    Update server/stats.go
    Co-Authored-By: Michael Kochell <mjkochell@gmail.com>

--- server/stats.go
+++ server/stats.go
@@ -184,7 +184,7 @@ func initUserCounter(currentInstanceStore CurrentInstanceStore, userStore UserSt
 
 var startedAt = time.Now()
 
-// EnsureUptime adds an "uptime" expvar
+// initUptime adds an "uptime" expvar
 func initUptime() {
 	goexpvar.Publish("uptime", goexpvar.Func(func() interface{} {
 		up := (time.Since(startedAt) + time.Second/2) / time.Second * time.Second
