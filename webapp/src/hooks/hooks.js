c7eb636b49103ca8746642c7a59921e87f5befe5
Michael Kochell <mjkochell@gmail.com>
2019-11-20 16:59:57 -0500

    [MM-18882] Remove ephemeral message for jira server and desktop… (#347)
    * remove ephemeral message for jira server desktop app combo
    
    support jira server popup only if desktop app version >= 4.3.0
    
    * merge issue, change wording of error
    
    * add example user agents
    
    * use `window.desktop.version`. change error message to point to docs
    
    * Change error to point user to desktop app download page
    
    Co-Authored-By: Aaron Rothschild <aaron.rothschild@gmail.com>

--- webapp/src/hooks/hooks.js
+++ webapp/src/hooks/hooks.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {isDesktopApp} from '../utils/user_agent';
+import {isDesktopApp, isMinimumDesktopAppVersion} from '../utils/user_agent';
 import {openCreateModalWithoutPost, openChannelSettings, sendEphemeralPost} from '../actions';
 import {isUserConnected, getInstalledInstanceType, isInstanceInstalled} from '../selectors';
 import PluginId from 'plugin_id';
@@ -40,8 +40,10 @@ export default class Hooks {
                 this.store.dispatch(sendEphemeralPost('You already have a Jira account linked to your Mattermost account. Please use `/jira disconnect` to disconnect.'));
                 return Promise.resolve({});
             }
-            if (getInstalledInstanceType(this.store.getState()) === 'server' && isDesktopApp()) {
-                this.store.dispatch(sendEphemeralPost('Please use your browser to connect to Jira.'));
+
+            if (getInstalledInstanceType(this.store.getState()) === 'server' && isDesktopApp() && !isMinimumDesktopAppVersion(4, 3, 0)) { // eslint-disable-line no-magic-numbers
+                const errMsg = 'Your version of the Mattermost desktop client does not support authenticating between Jira and Mattermost directly. To connect your Jira account with Mattermost, please go to Mattermost via your web browser and type `/jira connect`, or [check the Mattermost download page](https://mattermost.com/download/#mattermostApps) to get the latest version of the desktop client.';
+                this.store.dispatch(sendEphemeralPost(errMsg));
                 return Promise.resolve({});
             }
 
--- webapp/src/utils/user_agent.jsx
+++ webapp/src/utils/user_agent.jsx
@@ -1,8 +1,26 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {isMinimumServerVersion} from 'mattermost-redux/utils/helpers';
+
 const userAgent = window.navigator.userAgent;
 
 export function isDesktopApp() {
     return userAgent.indexOf('Mattermost') !== -1 && userAgent.indexOf('Electron') !== -1;
+}
+
+export function getDesktopAppVersion() {
+    if (window.desktop && window.desktop.version) {
+        return window.desktop.version;
+    }
+    return null;
+}
+
+export function isMinimumDesktopAppVersion(minMajorVersion, minMinorVersion, minDotVersion) {
+    const currentVersion = getDesktopAppVersion();
+    if (!currentVersion) {
+        return false;
+    }
+
+    return isMinimumServerVersion(currentVersion, minMajorVersion, minMinorVersion, minDotVersion);
 }
