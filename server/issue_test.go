a9477f7359fa9197e00ed68e73a45b90112ec0eb
Hector <hectordorito5@gmail.com>
2020-05-03 11:51:14 +0200

    basic unit tests

--- server/issue_test.go
+++ server/issue_test.go
@@ -20,6 +20,7 @@ import (
 const (
 	nonExistantIssueKey   = "FAKE-1"
 	noPermissionsIssueKey = "SUDO-1"
+	attachCommentErrorKey = "ATTACH-1"
 	existingIssueKey      = "REAL-1"
 	nonExistantProjectKey = "FP"
 	noIssueFoundError     = "We couldn't find the issue key. Please confirm the issue key and try again. You may not have permissions to access this issue."
@@ -57,6 +58,16 @@ func (client testClient) GetTransitions(issueKey string) ([]jira.Transition, err
 
 func (client testClient) DoTransition(issueKey string, transitionID string) error {
 	return nil
+}
+
+func (client testClient) AddComment(issueKey string, comment *jira.Comment) (*jira.Comment, error) {
+	if issueKey == noPermissionsIssueKey {
+		return nil, errors.New("you do not have the permission to comment on this issue")
+	} else if issueKey == attachCommentErrorKey {
+		return nil, errors.New("Unanticipated error")
+	}
+
+	return nil, nil
 }
 
 func TestTransitionJiraIssue(t *testing.T) {
@@ -193,4 +204,173 @@ func TestRouteIssueTransition(t *testing.T) {
 		})
 	}
 
+}
+
+func TestRouteAttachCommentToIssue(t *testing.T) {
+	api := &plugintest.API{}
+
+	api.On("LogError",
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string")).Return(nil)
+
+	api.On("LogDebug",
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string"),
+		mock.AnythingOfTypeArgument("string")).Return(nil)
+
+	siteURL := "https://somelink.com"
+	api.On("GetConfig").Return(&model.Config{ServiceSettings: model.ServiceSettings{SiteURL: &siteURL}})
+
+	api.On("GetPost", "error_post").Return(nil, &model.AppError{Id: "1"})
+	api.On("GetPost", "post_not_found").Return(nil, (*model.AppError)(nil))
+
+	api.On("GetPost", "0").Return(&model.Post{UserId: "0"}, (*model.AppError)(nil))
+	api.On("GetUser", "0").Return(nil, &model.AppError{Id: "1"})
+
+	api.On("GetPost", "1").Return(&model.Post{UserId: "1"}, (*model.AppError)(nil))
+	api.On("GetUser", "1").Return(&model.User{Username: "username"}, (*model.AppError)(nil))
+
+	api.On("CreatePost", mock.AnythingOfType("*model.Post")).Return(nil, (*model.AppError)(nil))
+
+	p := Plugin{}
+	p.SetAPI(api)
+
+	p.userStore = getMockUserStoreKV()
+	p.currentInstanceStore = mockCurrentInstanceStore{&p}
+
+	tests := map[string]struct {
+		method  string
+		header  string
+		request *struct {
+			PostId      string `json:"post_id"`
+			CurrentTeam string `json:"current_team"`
+			IssueKey    string `json:"issueKey"`
+		}
+		expectedCode int
+	}{
+		"Wrong method": {
+			method:       "GET",
+			header:       "",
+			request:      nil,
+			expectedCode: http.StatusMethodNotAllowed,
+		},
+		"No header": {
+			method:       "POST",
+			header:       "",
+			request:      nil,
+			expectedCode: http.StatusUnauthorized,
+		},
+		"User not found": {
+			method:       "POST",
+			header:       "nobody",
+			request:      nil,
+			expectedCode: http.StatusInternalServerError,
+		},
+		"Failed to load post": {
+			method: "POST",
+			header: "1",
+			request: &struct {
+				PostId      string `json:"post_id"`
+				CurrentTeam string `json:"current_team"`
+				IssueKey    string `json:"issueKey"`
+			}{
+				PostId: "error_post",
+			},
+			expectedCode: http.StatusInternalServerError,
+		},
+		"Post not found": {
+			method: "POST",
+			header: "1",
+			request: &struct {
+				PostId      string `json:"post_id"`
+				CurrentTeam string `json:"current_team"`
+				IssueKey    string `json:"issueKey"`
+			}{
+				PostId: "post_not_found",
+			},
+			expectedCode: http.StatusInternalServerError,
+		},
+		"Post user not found": {
+			method: "POST",
+			header: "1",
+			request: &struct {
+				PostId      string `json:"post_id"`
+				CurrentTeam string `json:"current_team"`
+				IssueKey    string `json:"issueKey"`
+			}{
+				PostId: "0",
+			},
+			expectedCode: http.StatusInternalServerError,
+		},
+		"No permissions to comment on issue": {
+			method: "POST",
+			header: "1",
+			request: &struct {
+				PostId      string `json:"post_id"`
+				CurrentTeam string `json:"current_team"`
+				IssueKey    string `json:"issueKey"`
+			}{
+				PostId:   "1",
+				IssueKey: noPermissionsIssueKey,
+			},
+			expectedCode: http.StatusNotFound,
+		},
+		"Failed to attach the comment": {
+			method: "POST",
+			header: "1",
+			request: &struct {
+				PostId      string `json:"post_id"`
+				CurrentTeam string `json:"current_team"`
+				IssueKey    string `json:"issueKey"`
+			}{
+				PostId:   "1",
+				IssueKey: attachCommentErrorKey,
+			},
+			expectedCode: http.StatusInternalServerError,
+		},
+		"Succesfully created notification post": {
+			method: "POST",
+			header: "1",
+			request: &struct {
+				PostId      string `json:"post_id"`
+				CurrentTeam string `json:"current_team"`
+				IssueKey    string `json:"issueKey"`
+			}{
+				PostId:   "1",
+				IssueKey: existingIssueKey,
+			},
+			expectedCode: http.StatusOK,
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			bb, err := json.Marshal(tt.request)
+			assert.Nil(t, err)
+
+			request := httptest.NewRequest(tt.method, routeAPIAttachCommentToIssue, strings.NewReader(string(bb)))
+			request.Header.Add("Mattermost-User-Id", tt.header)
+			w := httptest.NewRecorder()
+			p.ServeHTTP(&plugin.Context{}, w, request)
+			assert.Equal(t, tt.expectedCode, w.Result().StatusCode, "no request data")
+		})
+	}
 }
