a46c06562e07aef67be246eb4f1449b2fb603a62
Jason Frerich <jason.frerich@mattermost.com>
2020-06-16 17:21:23 -0500

    fix

--- webapp/src/components/post_menu_actions/create_issue/create_issue.jsx
+++ webapp/src/components/post_menu_actions/create_issue/create_issue.jsx
@@ -11,12 +11,27 @@ import JiraIcon from 'components/icon';
 export default class CreateIssuePostMenuAction extends PureComponent {
     static propTypes = {
         isSystemMessage: PropTypes.bool.isRequired,
+        locale: PropTypes.string,
         open: PropTypes.func.isRequired,
         postId: PropTypes.string,
         userConnected: PropTypes.bool.isRequired,
         installedInstanceType: PropTypes.string.isRequired,
         isInstanceInstalled: PropTypes.bool.isRequired,
         sendEphemeralPost: PropTypes.func.isRequired,
+    };
+
+    static defaultTypes = {
+        locale: 'en',
+    };
+
+    getLocalizedTitle = () => {
+        const {locale} = this.props;
+        switch (locale) {
+        case 'es':
+            return 'Crear incidencia en Jira';
+        default:
+            return 'Create Jira Issue';
+        }
     };
 
     handleClick = (e) => {
