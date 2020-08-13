7003f3fe9d814767f77a83f58b9f36101c877dec
Jason Frerich <jason.frerich@mattermost.com>
2020-08-13 10:44:05 -0500

    add ability build with rudder write key from Env Variable

---
+++ build/custom.mk
@@ -0,0 +1,5 @@
+# Include custom targets and environment variables here
+ifndef MM_RUDDER_WRITE_KEY
+	MM_RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
+endif
+GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-jira/server/utils/telemetry.rudderWriteKey=$(MM_RUDDER_WRITE_KEY)"'
