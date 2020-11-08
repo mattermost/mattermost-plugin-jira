8465406d8b99b57507e162f291a813ddc4da277d
Carina Antunes <carina.oliveira.antunes@cern.ch>
2020-11-08 19:11:09 +0100

    update build folder

--- build/custom.mk
+++ build/custom.mk
@@ -1,5 +1,1 @@
-# Include custom targets and environment variables here
-ifndef MM_RUDDER_WRITE_KEY
-	MM_RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
-endif
-LDFLAGS += -X "github.com/mattermost/mattermost-plugin-jira/server/utils/telemetry.rudderWriteKey=$(MM_RUDDER_WRITE_KEY)"
+# Include custome targets and environment variables here
