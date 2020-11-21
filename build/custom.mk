5eccb89fc4a257ec103e7d808f1f22b290741163
Carina Antunes <carina.oliveira.antunes@cern.ch>
2020-11-21 20:44:28 +0100

    fix typo

--- build/custom.mk
+++ build/custom.mk
@@ -1,4 +1,4 @@
-# Include custome targets and environment variables here
+# Include custom targets and environment variables here
 ifndef MM_RUDDER_WRITE_KEY
 	MM_RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
 endif
