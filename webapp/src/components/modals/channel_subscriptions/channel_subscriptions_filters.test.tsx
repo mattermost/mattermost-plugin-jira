297ca07873127768002768399e21e1bf6440b8f0
Haardik Dharma <dharmahaardik08@gmail.com>
2021-01-14 00:35:23 +0530

    Update channel_subscriptions_filters.test.tsx

--- webapp/src/components/modals/channel_subscriptions/channel_subscriptions_filters.test.tsx
+++ webapp/src/components/modals/channel_subscriptions/channel_subscriptions_filters.test.tsx
@@ -9,9 +9,9 @@ import {useFieldForIssueMetadata} from 'testdata/jira-issue-metadata-helpers';
 import {FilterFieldInclusion} from 'types/model';
 import {getCustomFieldFiltersForProjects} from 'utils/jira_issue_metadata';
 
-import ChannelSubscriptionsFilters, {Props} from './channel_subscriptions_filters';
+import ChannelSubscriptionFilters, {Props} from './channel_subscriptions_filters';
 
-describe('components/ChannelSubscriptionsFilters', () => {
+describe('components/ChannelSubscriptionFilters', () => {
     const field = {
         hasDefaultValue: false,
         key: 'priority',
@@ -62,8 +62,8 @@ describe('components/ChannelSubscriptionsFilters', () => {
 
     test('should match snapshot', () => {
         const props = {...baseProps};
-        const wrapper = shallow<ChannelSubscriptionsFilters>(
-            <ChannelSubscriptionsFilters {...props}/>
+        const wrapper = shallow<ChannelSubscriptionFilters>(
+            <ChannelSubscriptionFilters {...props}/>
         );
 
         wrapper.setState({showCreateRow: true});
