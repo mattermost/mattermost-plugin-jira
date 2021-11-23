268b3e4f18b67fbe5f9c1d85b2054d50fba3ecd0
Carlos Tadeu Panato Junior <carlos@mattermost.com>
2021-11-23 09:58:46 +0100

    dist: build linux/darwin arm64 targets (#822)

--- server/stats_test.go
+++ server/stats_test.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/mattermost/mattermost-server/v5/model"
-	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/mattermost/mattermost-server/v6/model"
+	"github.com/mattermost/mattermost-server/v6/plugin/plugintest"
 
 	"github.com/mattermost/mattermost-plugin-jira/server/expvar"
 )
