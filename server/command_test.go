e561667eab1dd4bee3d1f63ad5101c8c0e0cb5c0
Lev <1187448+levb@users.noreply.github.com>
2022-02-16 06:06:16 -0800

    Update server/command_test.go
    Co-authored-by: Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>

--- server/command_test.go
+++ server/command_test.go
@@ -321,7 +321,7 @@ func TestPlugin_ExecuteCommand_Installation(t *testing.T) {
 		"install inaccessible server instance": {
 			numInstances:      0,
 			commandArgs:       &model.CommandArgs{Command: "/jira install server https://inaccessible-jira-server.com", UserId: mockUserIDSysAdmin},
-			expectedMsgPrefix: `it looks like we couldn't validate the connection to your Jira server. Please make sure the URL was entered correctly. This could also be because of existing firewall or proxy rules. If you intend to have a one way integration from Jira to Mattermost this is not an issue: Jira server returned http status code "404" when checking for availability: "https://inaccessible-jira-server.com"`,
+			expectedMsgPrefix: `we couldn't validate the connection to your Jira server. This could be because of existing firewall or proxy rules or because the URL was entered incorrectly. If you intend to have a one-way integration from Jira to Mattermost this is not an issue: Jira server returned http status code "404" when checking for availability: "https://inaccessible-jira-server.com"`,
 		},
 		"install valid server instance in wrong state": {
 			numInstances:      0,
