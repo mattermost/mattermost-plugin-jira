75738b59503793c3c81f348a3a0c8b0814c971b2
Lev <1187448+levb@users.noreply.github.com>
2022-02-16 06:05:59 -0800

    Update server/command_test.go
    Co-authored-by: Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>

--- server/command_test.go
+++ server/command_test.go
@@ -306,7 +306,7 @@ func TestPlugin_ExecuteCommand_Installation(t *testing.T) {
 		"install valid cloud instance with broken json response": {
 			numInstances:      0,
 			commandArgs:       &model.CommandArgs{Command: "/jira install cloud https://broken-json-response.com", UserId: mockUserIDSysAdmin},
-			expectedMsgPrefix: "it looks like we couldn't validate the connection to your Jira server. Please make sure the URL was entered correctly. This could also be because of existing firewall or proxy rules. If you intend to have a one way integration from Jira to Mattermost this is not an issue: invalid character '}' looking for beginning of value",
+			expectedMsgPrefix: "we couldn't validate the connection to your Jira server. This could be because of existing firewall or proxy rules, or because the URL was entered incorrectly. If you intend to have a one-way integration from Jira to Mattermost this is not an issue: invalid character '}' looking for beginning of value",
 		},
 		"install valid server instance 1 preinstalled": {
 			numInstances:      1,
