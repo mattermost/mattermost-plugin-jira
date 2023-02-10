7a5c6162b49c36cffd33d61cf8ad38152c17b1ae
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2023-02-10 13:38:33 -0500

    Improve error handling in `OnSendDailyTelemetry` (#913)
    Automatic Merge

--- server/plugin.go
+++ server/plugin.go
@@ -519,40 +519,44 @@ func (p *Plugin) OnSendDailyTelemetry() {
 	instances, err := p.instanceStore.LoadInstances()
 	if err != nil {
 		p.API.LogWarn("Failed to get instances for telemetry", "error", err)
-	}
-	for _, id := range instances.IDs() {
-		switch instances.Get(id).Type {
-		case ServerInstanceType:
-			server++
-		case CloudInstanceType:
-			cloud++
+	} else {
+		for _, id := range instances.IDs() {
+			switch instances.Get(id).Type {
+			case ServerInstanceType:
+				server++
+			case CloudInstanceType:
+				cloud++
+			}
 		}
-	}
-	args["instance_count"] = server + cloud
-	if server > 0 {
-		args["server_instance_count"] = server
-	}
-	if cloud > 0 {
-		args["cloud_instance_count"] = cloud
+		args["instance_count"] = server + cloud
+		if server > 0 {
+			args["server_instance_count"] = server
+		}
+		if cloud > 0 {
+			args["cloud_instance_count"] = cloud
+		}
+
+		// Subscriptions
+		numSubscriptions := 0
+		var subs *Subscriptions
+		for _, id := range instances.IDs() {
+			subs, err = p.getSubscriptions(id)
+			if err != nil {
+				p.API.LogWarn("Failed to get subscriptions for telemetry", "error", err)
+			}
+			numSubscriptions += len(subs.Channel.ByID)
+		}
+
+		args["subscriptions"] = numSubscriptions
 	}
 
 	// Connected users
 	connected, err := p.userStore.CountUsers()
 	if err != nil {
 		p.API.LogWarn("Failed to get the number of connected users for telemetry", "error", err)
+	} else {
+		args["connected_user_count"] = connected
 	}
-	args["connected_user_count"] = connected
-
-	// Subscriptions
-	subscriptions := 0
-	for _, id := range instances.IDs() {
-		subs, err := p.getSubscriptions(id)
-		if err != nil {
-			p.API.LogWarn("Failed to get subscriptions for telemetry", "error", err)
-		}
-		subscriptions += len(subs.Channel.ByID)
-	}
-	args["subscriptions"] = subscriptions
 
 	_ = p.tracker.TrackEvent("stats", args)
 }
