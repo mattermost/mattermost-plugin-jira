1e1020489c45a3b9043e9825666aff2fd9292d09
Ben Schumacher <ben.schumacher@mattermost.com>
2023-03-21 20:10:32 +0100

    [MM-51269] Log errors from telemetry events (#922)

--- server/setup_flow.go
+++ server/setup_flow.go
@@ -594,8 +594,8 @@ func (p *Plugin) submitCreateServerInstance(f *flow.Flow, submission map[string]
 	}, nil, nil
 }
 
-func (p *Plugin) trackSetupWizard(key string, args map[string]interface{}) func(f *flow.Flow) {
+func (p *Plugin) trackSetupWizard(event string, args map[string]interface{}) func(f *flow.Flow) {
 	return func(f *flow.Flow) {
-		p.trackWithArgs(key, f.UserID, args)
+		p.TrackUserEvent(event, f.UserID, args)
 	}
 }
