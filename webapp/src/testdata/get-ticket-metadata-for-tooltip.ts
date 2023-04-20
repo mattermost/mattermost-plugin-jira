9046d1315932f13e6c030df878d9733af69588de
raghavaggarwal2308 <raghav.aggarwal@brightscout.com>
2023-04-20 20:59:55 +0530

    [MI-2988] Review fix

--- webapp/src/testdata/get-ticket-metadata-for-tooltip.ts
+++ webapp/src/testdata/get-ticket-metadata-for-tooltip.ts
@@ -1,36 +1,34 @@
 import {IssueAction} from 'types/tooltip';
 
-export const ticketData = (assigneeName: string | null): IssueAction => {
-    return {
-        data: {
-            key: 'ABC-123',
-            fields: {
-                assignee: {
-                    displayName: assigneeName || '',
-                    avatarUrls: assigneeName ? {
-                        '48x48': 'https://something.atlassian.net/avatar.png',
-                        '16x16': 'https://something.atlassian.net/avatar.png',
-                        '24x24': 'https://something.atlassian.net/avatar.png',
-                        '36x36': 'https://something.atlassian.net/avatar.png',
-                    } : '',
-                },
-                labels: ['label1', 'label2'],
-                description: 'This is a test description',
-                summary: 'This is a test summary',
-                project: {
-                    avatarUrls: {
-                        '48x48': 'https://something.atlassian.net/project.png',
-                    },
-                },
-                versions: ['Version 1.0', 'Version 2.0'],
-                status: {
-                    name: 'In Progress',
-                },
-                issuetype: {
-                    iconUrl: 'https://something.atlassian.net/issuetype.png',
+export const ticketData = (assigneeName: string | null): IssueAction => ({
+    data: {
+        key: 'ABC-123',
+        fields: {
+            assignee: {
+                displayName: assigneeName || '',
+                avatarUrls: assigneeName ? {
+                    '48x48': 'https://something.atlassian.net/avatar.png',
+                    '16x16': 'https://something.atlassian.net/avatar.png',
+                    '24x24': 'https://something.atlassian.net/avatar.png',
+                    '36x36': 'https://something.atlassian.net/avatar.png',
+                } : '',
+            },
+            labels: ['label1', 'label2'],
+            description: 'This is a test description',
+            summary: 'This is a test summary',
+            project: {
+                avatarUrls: {
+                    '48x48': 'https://something.atlassian.net/project.png',
                 },
             },
+            versions: ['Version 1.0', 'Version 2.0'],
+            status: {
+                name: 'In Progress',
+            },
+            issuetype: {
+                iconUrl: 'https://something.atlassian.net/issuetype.png',
+            },
         },
-        type: 'mockType',
-    };
-};
+    },
+    type: 'mockType',
+});
