70078a0b2621a6f5b8b3094536bf1038ca25c95e
Raghav Aggarwal <raghav.aggarwal@brightscout.com>
2023-11-20 08:15:01 +0530

    [MI-3635] Updated code to display proper errors on UI: (#986)
    1. Updated the backend code to return proper errors instead of empty response for the "get-search-autocomplete-fields" and "get-search-users" API.
    2. Updated UI code to display a generic error in case of "get-search-autocomplete-fields" and "get-search-users" API calls.

--- server/autocomplete_search.go
+++ server/autocomplete_search.go
@@ -22,29 +22,27 @@ func (p *Plugin) httpGetAutoCompleteFields(w http.ResponseWriter, r *http.Reques
 
 	client, _, _, err := p.getClient(types.ID(instanceID), types.ID(mattermostUserID))
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return respondErr(w, http.StatusInternalServerError, err)
 	}
 
 	results, err := client.SearchAutoCompleteFields(params)
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return respondErr(w, http.StatusInternalServerError, err)
 	}
 
 	if results == nil {
-		return http.StatusInternalServerError, errors.New("failed to return any results")
+		return respondErr(w, http.StatusInternalServerError, errors.New("failed to return any results"))
 	}
 
 	bb, err := json.Marshal(results)
 	if err != nil {
-		return http.StatusInternalServerError,
-			errors.WithMessage(err, "failed to marshal response")
+		return respondErr(w, http.StatusInternalServerError, errors.WithMessage(err, "failed to marshal response"))
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	_, err = w.Write(bb)
 	if err != nil {
-		return http.StatusInternalServerError,
-			errors.WithMessage(err, "failed to write response")
+		return respondErr(w, http.StatusInternalServerError, errors.WithMessage(err, "failed to write response"))
 	}
 	return http.StatusOK, nil
 }
@@ -57,30 +55,28 @@ func (p *Plugin) httpGetSearchUsers(w http.ResponseWriter, r *http.Request) (int
 
 	client, _, _, err := p.getClient(types.ID(instanceID), types.ID(mattermostUserID))
 	if err != nil {
-		return http.StatusInternalServerError, err
+		return respondErr(w, http.StatusInternalServerError, err)
 	}
 
 	// Get list of assignable users
 	jiraUsers, err := client.SearchUsersAssignableInProject(projectKey, userSearch, 10)
 	if StatusCode(err) == 401 {
-		return http.StatusInternalServerError, err
+		return respondErr(w, http.StatusInternalServerError, err)
 	}
 
 	if jiraUsers == nil {
-		return http.StatusInternalServerError, errors.New("failed to return any results")
+		return respondErr(w, http.StatusInternalServerError, errors.New("failed to return any results"))
 	}
 
 	bb, err := json.Marshal(jiraUsers)
 	if err != nil {
-		return http.StatusInternalServerError,
-			errors.WithMessage(err, "failed to marshal response")
+		return respondErr(w, http.StatusInternalServerError, errors.WithMessage(err, "failed to marshal response"))
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	_, err = w.Write(bb)
 	if err != nil {
-		return http.StatusInternalServerError,
-			errors.WithMessage(err, "failed to write response")
+		return http.StatusInternalServerError, errors.WithMessage(err, "failed to write response")
 	}
 	return http.StatusOK, nil
 }
