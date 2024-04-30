ed1d33e3c0674ad641c48c5cebf332c49c275e50
kshitij <kshitij.katiyar@brightscout.com>
2024-04-30 15:00:19 +0530

    [GH-1007]: Updated the instruction in the setup flow for the cloud oauth2.0

--- server/setup_flow.go
+++ server/setup_flow.go
@@ -323,7 +323,9 @@ func (p *Plugin) stepCloudOAuthConfigure() flow.Step {
 			"       {{.OAuthCompleteURL}}\n"+
 			"8. Select **Settings** in the left menu.\n"+
 			"9. Copy the **Client ID** and **Secret** and keep it handy.\n"+
-			"10. Click on the **Configure** button below, enter these details and then **Continue**.", JiraScopes)).
+			"10. By default the app will be listed as private. If you wish to make it public means other users can use it too then Select the **Distribution** in the left menu.\n"+
+			"11. Click on the **Sharing** radio button, fill the form with the relevant data and click on **Save changes**.\n"+
+			"12. Click on the **Configure** button below, enter these details and then **Continue**.", JiraScopes)).
 		WithButton(flow.Button{
 			Name:  "Configure",
 			Color: flow.ColorPrimary,
