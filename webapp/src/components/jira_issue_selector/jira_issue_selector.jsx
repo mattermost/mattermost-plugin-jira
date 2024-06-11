df7ea009396ef5c7e0da50d63a994a7b7f74c7cc
Shwetas Dhake <shwetas.dhake@mitaoe.ac.in>
2024-06-12 00:07:19 +0530

    Update jira_issue_selector.jsx
    1) Implemented Truncation for Long Issue Titles
    2) Added Full Title Display on Hover:
    3) Improved Error Handling and Validation Feedback:

--- webapp/src/components/jira_issue_selector/jira_issue_selector.jsx
+++ webapp/src/components/jira_issue_selector/jira_issue_selector.jsx
@@ -1,162 +1,164 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import debounce from 'debounce-promise';
-import AsyncSelect from 'react-select/async';
+import debounce from "debounce-promise";
+import AsyncSelect from "react-select/async";
 
-import {getStyleForReactSelect} from 'utils/styles';
+import { getStyleForReactSelect } from "utils/styles";
 
 const searchDebounceDelay = 400;
 
 export default class JiraIssueSelector extends Component {
-    static propTypes = {
-        required: PropTypes.bool,
-        theme: PropTypes.object.isRequired,
-        onChange: PropTypes.func.isRequired,
-        searchIssues: PropTypes.func.isRequired,
-        error: PropTypes.string,
-        value: PropTypes.string,
-        addValidate: PropTypes.func.isRequired,
-        removeValidate: PropTypes.func.isRequired,
-        instanceID: PropTypes.string.isRequired,
+  static propTypes = {
+    required: PropTypes.bool,
+    theme: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired,
+    searchIssues: PropTypes.func.isRequired,
+    error: PropTypes.string,
+    value: PropTypes.string,
+    addValidate: PropTypes.func.isRequired,
+    removeValidate: PropTypes.func.isRequired,
+    instanceID: PropTypes.string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = { invalid: false };
+  }
+
+  componentDidMount() {
+    if (this.props.addValidate) {
+      this.props.addValidate(this.isValid);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.removeValidate) {
+      this.props.removeValidate(this.isValid);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.invalid && this.props.value !== prevProps.value) {
+      this.setState({ invalid: false }); //eslint-disable-line react/no-did-update-set-state
+    }
+  }
+
+  handleIssueSearchTermChange = (inputValue) => {
+    return this.debouncedSearchIssues(inputValue);
+  };
+
+  searchIssues = (text) => {
+    const params = {
+      fields: "key,summary",
+      q: text.trim(),
+      instance_id: this.props.instanceID,
     };
 
-    constructor(props) {
-        super(props);
-
-        this.state = {invalid: false};
-    }
-
-    componentDidMount() {
-        if (this.props.addValidate) {
-            this.props.addValidate(this.isValid);
-        }
-    }
-
-    componentWillUnmount() {
-        if (this.props.removeValidate) {
-            this.props.removeValidate(this.isValid);
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.invalid && this.props.value !== prevProps.value) {
-            this.setState({invalid: false}); //eslint-disable-line react/no-did-update-set-state
-        }
-    }
-
-    handleIssueSearchTermChange = (inputValue) => {
-        return this.debouncedSearchIssues(inputValue);
-    };
-
-    searchIssues = (text) => {
-        const params = {
-            fields: 'key,summary',
-            q: text.trim(),
-            instance_id: this.props.instanceID,
-        };
-
-        return this.props.searchIssues(params).then(({data}) => {
-            return data.map((issue) => ({
-                value: issue.key,
-                label: `${issue.key}: ${issue.fields.summary}`,
-            }));
-        }).catch((e) => {
-            this.setState({error: e});
+    return this.props
+      .searchIssues(params)
+      .then(({ data }) => {
+        return data.map((issue) => {
+          const maxLength = 50; // Maximum length for the tooltip text
+          const truncatedSummary =
+            issue.fields.summary.length > maxLength
+              ? `${issue.fields.summary.substring(0, maxLength)}...`
+              : issue.fields.summary;
+          return {
+            value: issue.key,
+            label: `${issue.key}: ${truncatedSummary}`,
+            title: issue.fields.summary, // Set the full title as the tooltip text
+          };
         });
-    };
+      })
+      .catch((e) => {
+        this.setState({ error: e });
+      });
+  };
 
-    debouncedSearchIssues = debounce(this.searchIssues, searchDebounceDelay);
+  debouncedSearchIssues = debounce(this.searchIssues, searchDebounceDelay);
 
-    onChange = (e) => {
-        const value = e ? e.value : '';
-        this.props.onChange(value);
-    };
+  onChange = (e) => {
+    const value = e ? e.value : "";
+    this.props.onChange(value);
+  };
 
-    isValid = () => {
-        if (!this.props.required) {
-            return true;
-        }
+  isValid = () => {
+    if (!this.props.required) {
+      return true;
+    }
 
-        const valid = this.props.value && this.props.value.toString().length !== 0;
-        this.setState({invalid: !valid});
-        return valid;
-    };
+    const valid = this.props.value && this.props.value.toString().length !== 0;
+    this.setState({ invalid: !valid });
+    return valid;
+  };
 
-    render = () => {
-        const {error} = this.props;
-        const requiredStar = (
-            <span
-                className={'error-text'}
-                style={{marginLeft: '3px'}}
-            >
-                {'*'}
-            </span>
-        );
+  render = () => {
+    const { error } = this.props;
+    const requiredStar = (
+      <span className={"error-text"} style={{ marginLeft: "3px" }}>
+        {"*"}
+      </span>
+    );
 
-        let issueError = null;
-        if (error) {
-            issueError = (
-                <p className='help-text error-text'>
-                    <span>{error}</span>
-                </p>
-            );
-        }
+    let issueError = null;
+    if (error) {
+      issueError = (
+        <p className="help-text error-text">
+          <span>{error}</span>
+        </p>
+      );
+    }
 
-        const serverError = this.state.error;
-        let errComponent;
-        if (this.state.error) {
-            errComponent = (
-                <p className='alert alert-danger'>
-                    <i
-                        className='fa fa-warning'
-                        title='Warning Icon'
-                    />
-                    <span> {serverError.toString()}</span>
-                </p>
-            );
-        }
+    const serverError = this.state.error;
+    let errComponent;
+    if (this.state.error) {
+      errComponent = (
+        <p className="alert alert-danger">
+          <i className="fa fa-warning" title="Warning Icon" />
+          <span> {serverError.toString()}</span>
+        </p>
+      );
+    }
 
-        const requiredMsg = 'This field is required.';
-        let validationError = null;
-        if (this.props.required && this.state.invalid) {
-            validationError = (
-                <p className='help-text error-text'>
-                    <span>{requiredMsg}</span>
-                </p>
-            );
-        }
+    const requiredMsg = "This field is required.";
+    let validationError = null;
+    if (this.props.required && this.state.invalid) {
+      validationError = (
+        <p className="help-text error-text">
+          <span>{requiredMsg}</span>
+        </p>
+      );
+    }
 
-        return (
-            <div className={'form-group less'}>
-                {errComponent}
-                <label
-                    className={'control-label'}
-                    htmlFor={'issue'}
-                >
-                    {'Jira Issue'}
-                </label>
-                {this.props.required && requiredStar}
-                <AsyncSelect
-                    name={'issue'}
-                    placeholder={'Search for issues containing text...'}
-                    onChange={this.onChange}
-                    required={true}
-                    disabled={false}
-                    isMulti={false}
-                    isClearable={true}
-                    defaultOptions={true}
-                    loadOptions={this.handleIssueSearchTermChange}
-                    menuPortalTarget={document.body}
-                    menuPlacement='auto'
-                    styles={getStyleForReactSelect(this.props.theme)}
-                />
-                {validationError}
-                {issueError}
-            </div>
-        );
-    };
+    return (
+      <div className={"form-group less"}>
+        {errComponent}
+        <label className={"control-label"} htmlFor={"issue"}>
+          {"Jira Issue"}
+        </label>
+        {this.props.required && requiredStar}
+        <AsyncSelect
+          name={"issue"}
+          placeholder={"Search for issues containing text..."}
+          onChange={this.onChange}
+          required={true}
+          disabled={false}
+          isMulti={false}
+          isClearable={true}
+          defaultOptions={true}
+          loadOptions={this.handleIssueSearchTermChange}
+          menuPortalTarget={document.body}
+          menuPlacement="auto"
+          styles={getStyleForReactSelect(this.props.theme)}
+        />
+        {validationError}
+        {issueError}
+      </div>
+    );
+  };
 }
