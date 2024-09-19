069b8ab18d5a2e4ed2cb51f9cef921b17f782269
kshitij katiyar <kshitij.katiyar@brightscout.com>
2024-09-19 18:12:41 +0530

    [MM-1108]: Handled the truncation of issue summary while making it a link

--- server/issue_parser.go
+++ server/issue_parser.go
@@ -21,7 +21,7 @@ func parseJiraLinksToMarkdown(text string) string {
 }
 
 func mdKeySummaryLink(issue *jira.Issue, instance Instance) string {
-	return fmt.Sprintf("[%s: %s (%s)](%s%s)", issue.Key, issue.Fields.Summary, issue.Fields.Status.Name, instance.GetJiraBaseURL(), "/browse/"+issue.Key)
+	return fmt.Sprintf("[%s: %s (%s)](%s%s)", issue.Key, truncate(issue.Fields.Summary, 80), issue.Fields.Status.Name, instance.GetJiraBaseURL(), "/browse/"+issue.Key)
 }
 
 func reporterSummary(reporter *jira.User) string {
