cb79300bcc84f61c9b4acdb1d97f09ab4836886d
Jesse Hallam <jesse.hallam@gmail.com>
2025-02-03 12:15:05 -0400

    Apply and enforce file-level license headers (#1150)
    * Apply and enforce file-level license headers
    
    Use `mattermost-govet` and `eslint`'s `header/header` to ensure each
    file begins with the appropriate header, then fix all instances.
    
    ```
    // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
    // See LICENSE.txt for license information.
    ```
    
    Note that this renames `LICENSE` to `LICENSE.txt` purely to align on the
    copyright messaging across repos.
    
    * switch to 2017
    
    * bundle LICENSE.txt and NOTICE.txt if they exist
    
    * updated NOTICE.txt and configuration
    
    * rm duplicate Copyrights

--- webapp/src/utils/jira_issue_metadata.test.tsx
+++ webapp/src/utils/jira_issue_metadata.test.tsx
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 import createMeta from 'testdata/cloud-get-create-issue-metadata-for-project-many-fields.json';
