ddd5ff9e4dc11ece0db287bebec6d41a6c6c5fae
kshitij katiyar <90389917+Kshitij-Katiyar@users.noreply.github.com>
2025-03-06 12:54:04 +0530

    [MM-794]: Fixed the issue of fields not prefilled when creating issue with custom fields (#1127)
    * [MM-794]: Fixed the prefilled fields wehn creating issue with custom fields
    
    * [MM-794]: review fixes
    
    * [MM-794]: Added comment for jira custom field handling
    
    ---------
    
    Co-authored-by: Raghav Aggarwal <raghav.aggarwal@brightscout.com>
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- server/client.go
+++ server/client.go
@@ -359,14 +359,30 @@ func MakeCreateIssueURL(instance Instance, project *jira.Project, issue *jira.Is
 	}
 
 	// add custom fields
-	for k, v := range issue.Fields.Unknowns {
-		strV, ok := v.(string)
-		if ok {
-			q.Add(k, strV)
-		}
-		if mapV, ok := v.(map[string]interface{}); ok {
-			if id, ok := mapV["id"].(string); ok {
-				q.Add(k, id)
+	for key, field := range issue.Fields.Unknowns {
+		switch fieldTyped := field.(type) {
+		case string: // handles fields like url, short text, paragraph
+			q.Add(key, fieldTyped)
+		case map[string]interface{}: // handles fields like dropdown
+			if id, ok := fieldTyped["id"].(string); ok {
+				q.Add(key, id)
+			}
+		case []interface{}: // handles fields like labels, checkbox, flags
+			for _, element := range fieldTyped {
+				switch elementTyped := element.(type) {
+				case string:
+					q.Add(key, elementTyped)
+				case map[string]interface{}:
+					if id, ok := elementTyped["id"].(string); ok {
+						q.Add(key, id)
+					}
+				case []map[string]interface{}:
+					for _, mapWithID := range elementTyped {
+						if id, ok := mapWithID["id"].(string); ok {
+							q.Add(key, id)
+						}
+					}
+				}
 			}
 		}
 	}
