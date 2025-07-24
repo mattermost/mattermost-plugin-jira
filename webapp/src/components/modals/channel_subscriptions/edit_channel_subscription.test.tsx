8fb3944cd6d808effa9fb90860334575513e5c44
kshitij katiyar <90389917+Kshitij-Katiyar@users.noreply.github.com>
2025-07-24 19:35:10 +0530

    [GH-604]: Feature to create a subscription template for Jira subscriptions. (#897)
    * [MI-2353]:Updated create,get and delete subscription template APIs (#16)
    
    * before changes to tests
    
    * Lint and Test fixes
    
    * removed LogDebug messages
    
    * removed unused value
    
    * Merge remote-tracking branch 'upstream/master' into issue-604
    
    * removed unwanted change
    
    * [MI-2353]:Updated create,get and delete subscription template APIs
    
    * [MI-2353]:Fixed review fixes
    
    * [MI-2353]:Fixed review comments
    
    * [MI-2353]:Fixed review fixes
    
    * [MI-2353]:Added model constants
    
    * [MI-2369]:Created edit and get subscription template API (#17)
    
    * [MI-2369]:Created edit and get subscription template API
    
    * [MI-2369]:Fixed review fixes
    
    * [MI-2369]:added a condition
    
    Co-authored-by: Sanjay <sanjaymj@12345>
    Co-authored-by: sanjaydemansol <85980820+sanjaydemansol@users.noreply.github.com>
    Co-authored-by: sibasankarnayak <sibasankar@demansoltech.com>
    
    * [MM-604]:Integrated frontend code
    
    * [MM-604]:Fixed test cases
    
    * [MM-604]:Fixed naming convention
    
    * [MM-604]:Fixed lint error
    
    * [MM-604]:Fixed lint error
    
    * [MM-604]:Fixed lint erros
    
    * [MM-604]:Fixed merge conflicts
    
    * [MM-604]: fixed ci
    
    * [GH-604]: fixed the testcases
    
    * Update webapp/src/components/modals/channel_subscriptions/select_channel_subscription.tsx
    
    Co-authored-by: Doug Lauder <wiggin77@warpmail.net>
    
    * Fixed merge conflicts
    
    * Fixed CI
    
    * fix button css and added confirmation
    
    * fixed confirm modal not visible on  create screen
    
    * updated snapshot
    
    ---------
    
    Co-authored-by: Sanjay <sanjaymj@12345>
    Co-authored-by: sanjaydemansol <85980820+sanjaydemansol@users.noreply.github.com>
    Co-authored-by: sibasankarnayak <sibasankar@demansoltech.com>
    Co-authored-by: Raghav Aggarwal <raghav.aggarwal@brightscout.com>
    Co-authored-by: Doug Lauder <wiggin77@warpmail.net>
    Co-authored-by: Abbas Naqvi <abbas.naqvi@brightscout.com>

--- webapp/src/components/modals/channel_subscriptions/edit_channel_subscription.test.tsx
+++ webapp/src/components/modals/channel_subscriptions/edit_channel_subscription.test.tsx
@@ -22,6 +22,14 @@ describe('components/EditChannelSubscription', () => {
         deleteChannelSubscription: jest.fn().mockResolvedValue({}),
         editChannelSubscription: jest.fn().mockResolvedValue({}),
         fetchChannelSubscriptions: jest.fn().mockResolvedValue({}),
+        createSubscriptionTemplate: jest.fn().mockResolvedValue({}),
+        deleteSubscriptionTemplate: jest.fn().mockResolvedValue({}),
+        editSubscriptionTemplate: jest.fn().mockResolvedValue({}),
+        fetchAllSubscriptionTemplates: jest.fn().mockResolvedValue({}),
+        fetchSubscriptionTemplatesForProjectKey: jest.fn().mockResolvedValue({}),
+        sendEphemeralPost: jest.fn().mockResolvedValue({}),
+        getConnected: jest.fn().mockResolvedValue({}),
+        fetchJiraProjectMetadataForAllInstances: jest.fn().mockResolvedValue({}),
         fetchJiraIssueMetadataForProjects: jest.fn().mockResolvedValue({data: cloudIssueMetadata}),
     };
 
@@ -80,6 +88,7 @@ describe('components/EditChannelSubscription', () => {
         close: jest.fn(),
         selectedSubscription: channelSubscriptionForCloud,
         creatingSubscription: false,
+        creatingSubscriptionTemplate: false,
         securityLevelEmptyForJiraSubscriptions: true,
     };
 
@@ -549,7 +558,7 @@ describe('components/EditChannelSubscription', () => {
         wrapper.find('#jira-delete-subscription').simulate('click');
 
         expect(wrapper.state().showConfirmModal).toBe(true);
-        wrapper.instance().handleConfirmDelete();
+        wrapper.instance().handleConfirmAction();
 
         expect(deleteChannelSubscription).toHaveBeenCalled();
 
@@ -575,7 +584,7 @@ describe('components/EditChannelSubscription', () => {
         wrapper.find('#jira-delete-subscription').simulate('click');
 
         expect(wrapper.state().showConfirmModal).toBe(true);
-        wrapper.instance().handleConfirmDelete();
+        wrapper.instance().handleConfirmAction();
 
         expect(deleteChannelSubscription).toHaveBeenCalled();
 
--- webapp/src/components/modals/channel_subscriptions/select_channel_subscription.tsx
+++ webapp/src/components/modals/channel_subscriptions/select_channel_subscription.tsx
@@ -11,7 +11,9 @@ import {SharedProps} from './shared_props';
 
 type Props = SharedProps & {
     showEditChannelSubscription: (subscription: ChannelSubscription) => void;
+    showEditSubscriptionTemplate: (subscription: ChannelSubscription) => void;
     showCreateChannelSubscription: () => void;
+    showCreateSubscriptionTemplate: () => void;
     allProjectMetadata: AllProjectMetadata | null;
 };
 
@@ -19,6 +21,7 @@ type State = {
     error: string | null;
     showConfirmModal: boolean;
     subscriptionToDelete: ChannelSubscription | null;
+    isTemplate: boolean;
 }
 
 export default class SelectChannelSubscriptionInternal extends React.PureComponent<Props, State> {
@@ -26,6 +29,7 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
         error: null,
         showConfirmModal: false,
         subscriptionToDelete: null,
+        isTemplate: false,
     };
 
     handleCancelDelete = (): void => {
@@ -34,18 +38,31 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
 
     handleConfirmDelete = (): void => {
         this.setState({showConfirmModal: false});
-        this.deleteChannelSubscription(this.state.subscriptionToDelete);
+        if (this.state.isTemplate) {
+            this.deleteSubscriptionTemplate(this.state.subscriptionToDelete);
+        } else {
+            this.deleteChannelSubscription(this.state.subscriptionToDelete);
+        }
     };
 
-    handleDeleteChannelSubscription = (sub: ChannelSubscription): void => {
+    handleDeleteChannelSubscription = (sub: ChannelSubscription, isTemplate = false): void => {
         this.setState({
             showConfirmModal: true,
             subscriptionToDelete: sub,
+            isTemplate,
         });
     };
 
     deleteChannelSubscription = (sub: ChannelSubscription): void => {
         this.props.deleteChannelSubscription(sub).then((res: { error?: { message: string } }) => {
+            if (res.error) {
+                this.setState({error: res.error.message});
+            }
+        });
+    };
+
+    deleteSubscriptionTemplate = (sub: ChannelSubscription): void => {
+        this.props.deleteSubscriptionTemplate(sub).then((res: {error?: {message: string}}) => {
             if (res.error) {
                 this.setState({error: res.error.message});
             }
@@ -69,7 +86,7 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
         return projectKey;
     };
 
-    renderRow = (sub: ChannelSubscription): JSX.Element => {
+    renderRow = (sub: ChannelSubscription, forTemplates = false): JSX.Element => {
         const projectName = this.getProjectName(sub);
 
         const showInstanceColumn = this.props.installedInstances.length > 1;
@@ -77,6 +94,13 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
         const alias = this.props.installedInstances.filter((instance) => instance.instance_id === sub.instance_id)[0].alias;
         const instanceName = alias || sub.instance_id;
 
+        if (!forTemplates) {
+            return this.renderSubscriptionRow(sub, projectName, showInstanceColumn, instanceName);
+        }
+        return this.renderSubscriptionTemplateRow(sub, projectName, showInstanceColumn, instanceName);
+    };
+
+    renderSubscriptionRow(sub: ChannelSubscription, projectName: string, showInstanceColumn: boolean, instanceName: string): JSX.Element {
         return (
             <tr
                 key={sub.id}
@@ -113,11 +137,44 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
                 </td>
             </tr>
         );
-    };
+    }
+
+    renderSubscriptionTemplateRow(sub: ChannelSubscription, projectName: string, showInstanceColumn: boolean, instanceName: string): JSX.Element {
+        return (
+            <tr
+                key={sub.id}
+                className='select-channel-subscriptions-row'
+            >
+                <td>{sub.name || '(no name)'}</td>
+                <td>{projectName}</td>
+                {showInstanceColumn && (
+                    <td>{instanceName}</td>
+                )}
+
+                <td>
+                    <button
+                        className='style--none color--link'
+                        onClick={() => this.props.showEditSubscriptionTemplate(sub)}
+                        type='button'
+                    >
+                        {'Edit'}
+                    </button>
+                    {' - '}
+                    <button
+                        className='style--none color--link'
+                        onClick={() => this.handleDeleteChannelSubscription(sub, true)}
+                        type='button'
+                    >
+                        {'Delete'}
+                    </button>
+                </td>
+            </tr>
+        );
+    }
 
     render(): React.ReactElement {
-        const {channel, channelSubscriptions, omitDisplayName} = this.props;
-        const {error, showConfirmModal, subscriptionToDelete} = this.state;
+        const {channel, channelSubscriptions, subscriptionTemplates, omitDisplayName} = this.props;
+        const {error, showConfirmModal, subscriptionToDelete, isTemplate} = this.state;
 
         let errorDisplay = null;
         if (error) {
@@ -126,9 +183,10 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
             );
         }
 
-        let confirmDeleteMessage = 'Delete Subscription?';
+        let confirmDeleteMessage = '';
+        confirmDeleteMessage = `Are you sure to delete the subscription ${isTemplate ? 'template' : ''}?`;
         if (subscriptionToDelete && subscriptionToDelete.name) {
-            confirmDeleteMessage = `Delete Subscription "${subscriptionToDelete.name}"?`;
+            confirmDeleteMessage = `Are you sure to delete the subscription  ${isTemplate ? 'template' : ''} "${subscriptionToDelete.name}"?`;
         }
 
         let confirmModal = null;
@@ -143,7 +201,7 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
                     onCancel={this.handleCancelDelete}
                     onConfirm={this.handleConfirmDelete}
                     show={true}
-                    title={'Subscription'}
+                    title={isTemplate ? 'Subscription Template' : 'Subscription'}
                 />
             );
         }
@@ -153,21 +211,39 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
             titleMessage = <h2 className='text-center'>{'Jira Subscriptions'}</h2>;
         }
 
+        const subscriptionTemplateTitle = <h2 className='text-center'>{'Jira Subscription Templates'}</h2>;
         const showInstanceColumn = this.props.installedInstances.length > 1;
         let subscriptionRows;
+        let subscriptionTemplateRows;
+        const columns = (
+            <thead>
+                <tr>
+                    <th scope='col'>{'Name'}</th>
+                    <th
+                        className='th-col'
+                        scope='col'
+                    >{'Project'}</th>
+                    {showInstanceColumn &&
+                    <th
+                        className='th-col'
+                        scope='col'
+                    >{'Instance'}</th>
+                    }
+                    <th
+                        className='th-col'
+                        scope='col'
+                    >{'Actions'}</th>
+                </tr>
+            </thead>
+        );
         if (channelSubscriptions.length) {
             subscriptionRows = (
                 <table className='table'>
-                    <thead>
-                        <tr>
-                            <th scope='col'>{'Name'}</th>
-                            <th scope='col'>{'Project'}</th>
-                            {showInstanceColumn && <th scope='col'>{'Instance'}</th>}
-                            <th scope='col'>{'Actions'}</th>
-                        </tr>
-                    </thead>
+                    {columns}
                     <tbody>
-                        {channelSubscriptions.map(this.renderRow)}
+                        {channelSubscriptions.map((element) => (
+                            this.renderRow(element, false)
+                        ))}
                     </tbody>
                 </table>
             );
@@ -176,6 +252,23 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
                 <p>
                     {'Click "Create Subscription" to receive Jira issue notifications in this channel.'}
                 </p>
+            );
+        }
+
+        if (subscriptionTemplates.length) {
+            subscriptionTemplateRows = (
+                <table className='table'>
+                    {columns}
+                    <tbody>
+                        {subscriptionTemplates.map((element) => (
+                            this.renderRow(element, true)
+                        ))}
+                    </tbody>
+                </table>
+            );
+        } else {
+            subscriptionTemplateRows = (
+                <p>{'Click "Create Template" to create a subscription template.'}</p>
             );
         }
 
@@ -191,9 +284,23 @@ export default class SelectChannelSubscriptionInternal extends React.PureCompone
                         {'Create Subscription'}
                     </button>
                 </div>
+                {subscriptionRows}
+                <div
+                    className='d-flex align-items-center mb-3 w-100'
+                    style={{justifyContent: 'space-between'}}
+                >
+                    <div>{subscriptionTemplateTitle}</div>
+                    <button
+                        className='btn btn-primary'
+                        onClick={this.props.showCreateSubscriptionTemplate}
+                        type='button'
+                    >
+                        {'Create Template'}
+                    </button>
+                </div>
+                {subscriptionTemplateRows}
                 {confirmModal}
                 {errorDisplay}
-                {subscriptionRows}
             </div>
         );
     }
