cf13dcf543b8b790feea37cfbfb24b8e380d4cf8
Abbas Mehdi Naqvi <abbas.naqvi@brightscout.com>
2025-10-31 17:52:00 +0530

    Add team field to create issue modal (#1225)
    * Add team field to create issue modal
    
    * improved code
    
    * Update server/issue.go
    
    * Update server/issue.go
    
    * review fixes
    
    ---------
    
    Co-authored-by: Doug Lauder <wiggin77@warpmail.net>

--- server/issue.go
+++ server/issue.go
@@ -39,6 +39,8 @@ const (
 	QueryParamProjectID  = "project_id"
 
 	expandValueGroups = "groups"
+
+	teamFieldKey = "customfield_10001"
 )
 
 type CreateMetaInfo struct {
@@ -237,6 +239,8 @@ func (p *Plugin) CreateIssue(in *InCreateIssue) (*jira.Issue, error) {
 		Fields: &in.Fields,
 	}
 
+	issue.Fields = preProcessTeamID(issue.Fields, teamFieldKey)
+
 	channelID := in.ChannelID
 	if post != nil {
 		channelID = post.ChannelId
@@ -365,6 +369,20 @@ func (p *Plugin) CreateIssue(in *InCreateIssue) (*jira.Issue, error) {
 	return createdIssue, nil
 }
 
+// Extract the "id" value from the custom field map and replace it
+// with a plain string, since Jira expects the field value directly.
+func preProcessTeamID(fields *jira.IssueFields, teamFieldID string) *jira.IssueFields {
+	if raw, ok := fields.Unknowns[teamFieldID]; ok {
+		if m, ok := raw.(map[string]interface{}); ok {
+			if id, ok := m["id"].(string); ok {
+				fields.Unknowns[teamFieldID] = id
+			}
+		}
+	}
+
+	return fields
+}
+
 func (p *Plugin) httpGetCreateIssueMetadataForProjects(w http.ResponseWriter, r *http.Request) (int, error) {
 	mattermostUserID := r.Header.Get("Mattermost-User-Id")
 	projectKeys := r.FormValue("project-keys")
@@ -408,10 +426,52 @@ func (p *Plugin) GetCreateIssueMetadataForProjects(instanceID, mattermostUserID 
 		return nil, err
 	}
 
+	teamIDList := p.conf.TeamIDList
+	if len(teamIDList) > 0 {
+		injectTeamAllowedValues(metaInfo, teamIDList)
+	}
+
 	return &CreateMetaInfo{
 		metaInfo,
 		projectStatuses,
 	}, nil
+}
+
+// The go-jira package does not support the Team field by default,
+// and the Jira API does not include Team data in the issue metadata.
+// Therefore, we need to manually inject the allowed Team values into the fields.
+func injectTeamAllowedValues(metaInfo *jira.CreateMetaInfo, teamIDList []TeamList) {
+	allowedValues := make([]map[string]string, 0, len(teamIDList))
+	for _, team := range teamIDList {
+		allowedValues = append(allowedValues, map[string]string{
+			"id":    team.ID,
+			"name":  team.Name,
+			"value": team.Name,
+		})
+	}
+
+	for _, project := range metaInfo.Projects {
+		for _, issueType := range project.IssueTypes {
+			for key, rawField := range issueType.Fields {
+				fieldMap, ok := rawField.(map[string]any)
+				if !ok {
+					continue
+				}
+
+				schemaRaw, ok := fieldMap["schema"].(map[string]any)
+				if !ok {
+					continue
+				}
+
+				if schemaRaw["custom"] != "com.atlassian.jira.plugin.system.customfieldtypes:atlassian-team" {
+					continue
+				}
+
+				fieldMap["allowedValues"] = allowedValues
+				issueType.Fields[key] = fieldMap
+			}
+		}
+	}
 }
 
 func (p *Plugin) httpGetTeamFields(w http.ResponseWriter, r *http.Request) (int, error) {
