7f866078f27a188b1b2c4717502286e3421d671a
Andre Vasconcelos <a-andre.vasconcelos@mattermost.com>
2026-09-26 01:38:16 +0900

    Applying PR feedback

--- webapp/src/components/modals/channel_subscriptions/channel_subscription_filters.tsx
+++ webapp/src/components/modals/channel_subscriptions/channel_subscription_filters.tsx
@@ -25,10 +25,7 @@ export type Props = {
     onChange: (f: FilterValue[]) => void;
     instanceID: string;
     securityLevelEmptyForJiraSubscriptions?: boolean;
-    searchTeamFields: (params: {fieldValue: string; instance_id: string}) => Promise<{
-        data: {items: {Name: string; ID: string}[]};
-        error?: Error;
-    }>;
+    searchTeamFields: (params: {fieldValue: string; instance_id: string}) => Promise<{data: {name: string; id: string}[]}>;
     projectKey: string;
 };
 
